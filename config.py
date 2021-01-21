69a31a8936e1859919734c6e63465c5f92fdf6a9
cpajr <charlie.allenjr@gmail.com>
2021-01-21 14:17:51 -0700

    Skeleton config

---
+++ config.py
@@ -0,0 +1,9 @@
+'''
+Configuration file for netbox-dns.py
+'''
+
+api_token = ''
+api_url_host = ''
+api_url_base = '/api/extras/object-changes/?time_after='
+api_url_base_suffix = '&changed_object_type_id=47'
+zone_name = ''
