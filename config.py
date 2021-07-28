7beb9aa1d76e8e99a8b69e5dea405ee36219b3f8
cpajr <charlie.allenjr@gmail.com>
2021-07-28 13:23:07 -0600

    Initial

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 Configuration file for netbox-dns.py
 '''
 
-api_token = ''
+api_token = '5bd66dca1b59e1a430b9d12998e083c14828fb60'
 api_url_host = ''
 api_url_base = '/api/extras/object-changes/?time_after='
 api_url_base_suffix = '&changed_object_type_id=47'
